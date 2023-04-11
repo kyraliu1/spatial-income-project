bcc28ff300df204562fde7be5f18bea1a89f22a3
kyraliu1 <kyraliu@gmail.com>
2023-04-11 12:54:55 -0700

    first commit

---
+++ incomedataexplore.R
@@ -0,0 +1,40 @@
+wd <- "~/Documents/research/spatial-income-project" # kyra pop wd
+setwd(wd)
+library(terra)
+library(tidycensus)
+
+# getting aggregated migration data
+agmigfi <- "./opinsights/aggmigration"
+
+if (!file.exists(agmigfi)){
+  dir.create("./opinsights")
+  download.file("https://opportunityinsights.org/wp-content/uploads/2022/07/od_pooled.csv",
+                agmigfi)
+}
+aggmig <- read.csv(agmigfi)
+
+# getting parental income data
+outfi <- "./opinsights/parentalincome"
+if (!file.exists(outfi)){
+  
+  download.file("https://opportunityinsights.org/wp-content/uploads/2018/10/cz_outcomes.csv",
+                outfi)
+}
+# acs.var$concept[acs.var$name == vari[2]]
+parental <- read.csv(outfi)
+
+# getting commuter zone shapefile
+comzoneshape <- "./opinsights/commuterzones/cz1990"
+if (!file.exists(comzoneshape)){
+  dir.create("./opinsights/commuterzones")
+  download.file(  "https://opportunityinsights.org/wp-content/uploads/2018/07/cz1990_shapefile.zip",
+                  comzoneshape)
+}
+unzip(comzoneshape,exdir ="./opinsights/commuterzones" )
+comzones <- vect("./opinsights/commuterzones/cz1990.shp")
+comzones = crop(comzones, c(-135,-50,20,50))
+
+# merging commuter zones to parental income 
+czonesparental <- merge(comzones,parental,by="cz")
+plot(comzones)
+points(czonesparental, cex = 0.2)
