20f558102ea468588e5bde0641ba11dc2040e8fb
kyraliu1 <kyraliu@gmail.com>
2023-08-21 18:36:17 -0700

    getting historical boundaries

--- 9_nhgis_explore.R
+++ 9_nhgis_explore.R
@@ -37,11 +37,21 @@ race_tract = read.csv(list.files(nhdirs[1],pattern = 'tract.csv',full.names = T)
 # AJ: Persons: Two or More Races -- only 2000 & 2010
 
 
-tracts <- readRDS("./censusdata/acs5_2018_tract")
-tracts <- tracts[,1:2]
-
-tracts$NAME2010 <- sapply(strsplit(tracts$NAME, ','), function(x) x[1])
-
-race_tract$GEOID <- substr(race_tract$NHGISCODE,2,14)
+# tracts <- readRDS("./censusdata/acs5_2018_tract")
+# tracts <- tracts[,1:2]
+# 
+# tracts$NAME2010 <- sapply(strsplit(tracts$NAME, ','), function(x) x[1])
+# 
+# race_tract$GEOID <- substr(race_tract$NHGISCODE,2,14)
 
 #rt <- merge(tracts,race_tract,'NAME2010')
+
+sfi <- list.files('./ipums',pattern = 'shape',full.names = T)
+sfi <- unzip(sfi,exdir = './ipums/bounds')
+
+unz <- sapply(sfi,  FUN = 'unzip',exdir = './ipums/bounds')
+
+sfi <- list.files('./ipums/bounds',pattern = '.shp$',full.names = T)
+
+tract1970 = vect(sfi[7])
+plot(tract1970)
